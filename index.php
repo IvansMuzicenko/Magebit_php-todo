da32af5492215a09a3ce77a7cbe528f94534e8b0
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-08-15 16:45:51 +0300

    feat: init

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+header('Content-Type: application/json');
+
+const DATA_FILE = "data.json";
+
+// url
+
+$data = [];
+if (file_exists(DATA_FILE)) {
+    $content = file_get_contents(DATA_FILE);
+    $data = json_decode($content, true);
+    if (!is_array($data)) {
+        $data = [];
+    }
+}
+
+
+$output = ['status' => false];
+
+if (isset($_GET)) {
+    $output['status'] = true;
+    $output['entries'] = $data;
+}
+
+if (isset($_POST)) {
+    if (isset($_POST["newItem"]) && !in_array($_POST["newItem"], $data)) {
+        $data[] = $_POST["newItem"];
+        $content = json_encode($data);
+        file_put_contents(DATA_FILE, $content);
+        $output = [
+            'status' => true,
+            'message' => 'new item added',
+            "data" => $content
+        ];
+    }
+    if (isset($_POST['removeItem'])) {
+        $data = array_flip($data);
+        unset($data[$_POST['removeItem']]);
+        $data = array_keys($data);
+        $content = json_encode($data);
+        file_put_contents(DATA_FILE, $content);
+        $output = [
+            'status' => true,
+            'message' => 'item removed',
+            "data" => $content
+        ];
+    }
+}
+echo json_encode($output, JSON_PRETTY_PRINT);
---
+++ src/app.js
@@ -0,0 +1,76 @@
+const myList = document.querySelector("#myList");
+const friendsList = document.querySelector(".friendsList");
+const inputGroup = document.querySelector(".inputGroup");
+const itemInput = document.querySelector("#itemInput");
+const addBtn = document.querySelector("#addBtn");
+let allItems = [];
+
+fetch("http://web.local/")
+  .then(async (response) => {
+    return await response.json();
+  })
+  .then(async (data) => {
+    if (data) {
+      allItems = await data.entries;
+    } else {
+      allItems = [];
+    }
+    allItems = allItems.map((el) => el.toString());
+    allItems.forEach((item) => addItem(item));
+  });
+
+const addItem = (item) => {
+  const newItem = document.createElement("li");
+  newItem.append(item);
+  myList.append(newItem);
+  const data = new FormData();
+  data.set("newItem", item);
+  fetch("http://web.local/", {
+    method: "POST",
+    body: data,
+  });
+};
+
+const validateItem = (item) => {
+  const allItemsBig = allItems.map((element) => element.toUpperCase());
+  if (allItemsBig.includes(item.toUpperCase())) return;
+  const firstLetter = item[0];
+  const fItem = item.replace(firstLetter, firstLetter.toUpperCase());
+  allItems.push(fItem);
+  addItem(fItem);
+};
+
+const clearInput = () => {
+  if (!itemInput.value) {
+    itemInput.placeholder = "Add your favorite item:";
+    addBtn.hidden = true;
+  }
+};
+
+inputGroup.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const itemInput = document.querySelector("#itemInput");
+  if (!itemInput.value) return null;
+  validateItem(itemInput.value);
+  itemInput.value = "";
+  clearInput();
+});
+
+myList.addEventListener("click", (e) => {
+  e.target.nodeName === "LI" && e.target.remove();
+  const targetItem = allItems.indexOf(e.target.innerText);
+  const data = new FormData();
+  data.set("removeItem", e.target.innerText);
+  fetch("http://web.local/", {
+    method: "POST",
+    body: data,
+  });
+  allItems.splice(targetItem, 1);
+});
+
+inputGroup.addEventListener("focusin", () => {
+  itemInput.placeholder = "";
+  addBtn.hidden = false;
+});
+
+inputGroup.addEventListener("focusout", () => clearInput());
